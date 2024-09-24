3cbad31d76bc05740536a4bd3c21916e02e134b1
informatica10 <alexteotonio@bioextratus.rede.bioextratus>
2024-09-24 16:25:16 -0300

    Nova aplicação

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import express from 'express'
-import { connection } from './database/connection.js'
-import routes from './routes/Routes.js'
-import path from 'path'
+import express from "express";
+import { connection } from "./database/connection.js";
+import routes from "./routes/Routes.js";
+import cors from "cors";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const app = express()
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000", // Permite apenas este domínio
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Métodos permitidos
+  allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
+  credentials: true, // Permite cookies e outras credenciais
+};
 
-const PORT = 3005
+app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use(routes)
+const PORT = 3005;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.urlencoded({ extended: true }))
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(express.json());
+app.use(routes);
+
+app.use(express.urlencoded({ extended: true }));
 
 // Configuração para servir arquivos estáticos
-app.use('/uploads', express.static(path.resolve('uploads')))
-
+app.use("/uploads", express.static(path.resolve("uploads")));
 
 // Conectar ao MongoDB
-connection().catch(console.error)
+connection().catch(console.error);
 
-app.listen(PORT , () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
--- src/routes/Router.Feedback.js
+++ src/routes/Router.Feedback.js
@@ -1,13 +1,24 @@
-import express from 'express'
-import ControllerFeedback from '../controller/ControllerFeedback.js'
-import upload from '../middlewares/multerConfig.js'
+import express from "express";
+import ControllerFeedback from "../controller/ControllerFeedback.js";
+import upload from "../middlewares/MulterConfig.js";
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/home', ControllerFeedback.getAllFeedback)
-routes.get('/home', ControllerFeedback.getOneFeedback)
-routes.post('/usuarios/addfeedback', upload.array('media', 5), ControllerFeedback.submitFeedback)
-routes.put('/usuarios/addfeedback/:_id', ControllerFeedback.updateFeedback)
-routes.delete('/usuarios/addfeedback/:_id', ControllerFeedback.deleteFeedback)
+routes.get("/home", ControllerFeedback.getAllFeedback);
+routes.get("/home", ControllerFeedback.getOneFeedback);
+routes.get("/meus-feedbacks/:idUsuario", ControllerFeedback.getFeedbackUsuario);
+routes.post(
+  "/addfeedback",
+  upload.array("media", 5),
+  ControllerFeedback.submitFeedback
+);
+routes.put(
+  "/atualizar/:id",
+  upload.array("media", 5),
+  ControllerFeedback.updateFeedback
+);
 
-export default routes
+routes.delete("/delete/:id", ControllerFeedback.deleteFeedback);
+routes.post("/likefeedbacks/:id/like", ControllerFeedback.likeFeedback);
+
+export default routes;
