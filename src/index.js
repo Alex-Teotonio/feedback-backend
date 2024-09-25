004b18b4b4674e74c2019585a5be980294abc6e3
Mona Lisa <taliafmendes09@gmail.com>
2024-09-25 10:49:09 -0300

    Criar pasta uploads , caso não exista

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import routes from "./routes/Routes.js";
 import cors from "cors";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 import dotenv from "dotenv";
 
@@ -17,6 +18,15 @@ const PORT = process.env.PORT;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const uploadsDir = path.join(__dirname, "..", "uploads");
+
+// Verificar e criar a pasta 'uploads' se não existir
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+  console.log(`Pasta 'uploads' criada em: ${uploadsDir}`);
+} else {
+  console.log(`Pasta 'uploads' já existe em: ${uploadsDir}`);
+}
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(express.json());
 app.use(routes);
